46caffffafb0aa7c6c42597d92476f41a457e546
Madi Apgar <132315455+madiapgar@users.noreply.github.com>
2025-06-23 09:52:09 -0600

    pipeline weekly updates

--- test_snake_sbatch.sh
+++ test_snake_sbatch.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 #SBATCH --nodes=1 # use one node
-#SBATCH --time=08:00:00 # 2h, madi edit: had to up the time so the job wasn't canceled
+#SBATCH --time=03:00:00 # 2h, madi edit: had to up the time so the job wasn't canceled
 #SBATCH --account=amc-general # normal, amc, long, mem (use mem when using the amem partition)
 #SBATCH --partition=amilan # amilian, ami100, aa100, amem, amc
 #SBATCH --qos=normal 
 #SBATCH --ntasks=1 # total processes/threads, madi edit: need n tasks to be the same as --threads in fastqc command
-#SBATCH --job-name=test_star_genome_index_gen
-#SBATCH --output=logs/06162025/test_star_index_%J.log
-#SBATCH --mem=5G # suffix K,M,G,T can be used with default to M, madi edit: uses <5G 
+#SBATCH --job-name=test_star_alignment_rule
+#SBATCH --output=logs/06182025/test_starAlignment_rule_%J.log
+#SBATCH --mem=1G # suffix K,M,G,T can be used with default to M, madi edit: uses <5G 
 #SBATCH --mail-user=MADISON.APGAR@CUANSCHUTZ.EDU
 #SBATCH --mail-type=FAIL,END
-#SBATCH --error=logs/06162025/test_star_index_%J.err
+#SBATCH --error=logs/06182025/test_starAlignment_rule_%J.err
 
 module load miniforge/24.11.3-0
 module load singularity/3.6.4
