8b0045de995bf6b799748f17b754249f7afa1e3e
Madi Apgar <132315455+madiapgar@users.noreply.github.com>
2025-07-14 08:43:34 -0600

    picard rules and calculating strandedness

--- workflow/snake_utils/snake_functions.py
+++ workflow/snake_utils/snake_functions.py
@@ -1,6 +1,9 @@
 ## attempting to put together overall functions to use in my pipeline
 from os.path import join as pj
 import pandas as pd
+from os.path import basename
+import re
+import glob
 
 def comb_filepaths(filepath1,
                    filepath2):
@@ -22,6 +25,35 @@ def make_fp_dict(metadata_df,
         raw_sample_dict_out.update({sample: proc_rawFile_list})
 
     return(raw_sample_dict_out)
+
+
+def picard_calculate_strandedness(file_pattern,
+                                  out_file):
+    picard_out_dict = {}
+
+    picard_collectRNAseq_files = [f for f in glob.glob(file_pattern)]
+
+    for file in picard_collectRNAseq_files:
+        picard_file = pd.read_csv(file, 
+                                sep="\t",
+                                skiprows=6,
+                                nrows=1)
+
+        file_name = basename(file)
+        sample = re.sub(".picard.metrics.txt", "", file_name)
+        picard_file["sampleid"] = sample
+
+        if (picard_file["PCT_R1_TRANSCRIPT_STRAND_READS"] >= 0.70).all():
+            picard_file["strandedness"] = "R1"
+        elif (picard_file["PCT_R2_TRANSCRIPT_STRAND_READS"] >= 0.70).all():
+            picard_file["strandedness"] = "R2"
+        else:
+            picard_file["strandedness"] = "unstranded"
+
+        picard_out_dict.update({f"{sample}": picard_file})
+
+    all_picard_res = pd.concat(picard_out_dict, ignore_index=True)
+    all_picard_res.to_csv(out_file)
 
 
 
