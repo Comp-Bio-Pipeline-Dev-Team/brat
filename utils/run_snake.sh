71e8326f8a1e68a80e5817bd558f410b4a72b2df
Madi Apgar <132315455+madiapgar@users.noreply.github.com>
2025-07-18 13:46:38 -0600

    bug fixes to pipeline

--- utils/run_snake.sh
+++ utils/run_snake.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
 ## put symlink ahead of this? - snakemake param where you want singularity images stored 
+## if [ -L "/scratch/alpine/$USER/apptainer_cache" ]; 
+##then
+##    echo "Singularity symlink already exists!"
+##else
+##    echo "Singularity symlink does not exist. Generating symlink..."
+##    ln -s /scratch/alpine/$USER/apptainer_cache ~/.singularity
+##fi
 
-snakeLog="logs/07172025/snake_log_07172025.log"
+snakeLog="logs/07182025/snake_log_07182025.log"
 
 snakemake \
     -s workflow/snakefile \
     --configfile workflow/config_files/test_config.yml \
     --jobs unlimited \
-    --workflow-profile workflow/profiles/default \
+    --workflow-profile workflow/profiles/debug \
     --software-deployment-method apptainer \
     --rerun-incomplete \
     --dry-run
     ##2>${snakeLog}
     ##--unlock
-    ##--dry-run
 
 
-    ##--unlock
     ##--singularity-args "-B /scratch/alpine/mapgar@xsede.org:/scratch/alpine/mapgar@xsede.org"
-    ##--slurm-keep-successful-logs
