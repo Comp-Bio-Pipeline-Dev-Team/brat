18e53ae2ca62c66a8fad9e3fdcab64f36ae8e2a2
Madi Apgar <132315455+madiapgar@users.noreply.github.com>
2026-01-28 15:18:14 -0700

    working on #36 and #40

--- utils/calc_job_eff.sh
+++ utils/calc_job_eff.sh
@@ -4,10 +4,10 @@ echo "gathering job efficiency stats...."
 
 ## relative to where you're calling the script from!! not where the script actually lives
 slurmLogs="logs/slurm/"
-jobStatsFile="logs/roberta_compGenome_jobs_07182025.csv"
+jobStatsFile="hyperthreaded_bratJobs_01152026.csv"
 
-python3 utils/madis_job_efficiency_v2.py --directory ${slurmLogs} \
-				      	 --out_file ${jobStatsFile}
-				      	 ##--keep_failed
+python3 madis_job_efficiency_v2.py --directory ${slurmLogs} \
+				      	 		   --out_file ${jobStatsFile}
+				      	 		   ##--keep_failed
 
 echo "saving job efficiency stats to ${jobStatsFile}!"
